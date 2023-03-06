59b318d77ba265c148a27d168142704bca4f837d
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-03-06 09:53:27 -0800

    Create BasicInput.cpp

---
+++ BasicInput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "I can guess your age! Enter your age: " ;
+    int age;
+    cin >> age;
+    cout << "Voila! Your age is: " << age <<endl;
+    return 0;
+}
