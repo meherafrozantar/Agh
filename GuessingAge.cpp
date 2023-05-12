3b53719fcda09939cd0ddb281cd6856404afdfc1
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-05-12 08:53:09 -0700

    Create GuessingAge.cpp

---
+++ GuessingAge.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "I can guess your age. Enter your age." << endl;
+    int age;
+    cin >> age;
+    cout << "Volla! I know your age is: " << age << endl;
+
+    return 0;
+
+    return 0;
+}
