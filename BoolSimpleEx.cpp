fe34c13941b0bc481a18400dcf5fe40f16bb969e
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-05-12 08:43:14 -0700

    Create BoolSimpleEx.cpp

---
+++ BoolSimpleEx.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    bool happy = true;
+    cout << happy;
+    cout << '\n';
+    bool sad = false;
+    cout << sad;
+
+    return 0;
+}
