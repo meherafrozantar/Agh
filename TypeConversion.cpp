5ab355a2927af51202e016ee06dd857846f34264
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-05-24 01:14:38 -0700

    Create TypeConversion.cpp

---
+++ TypeConversion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a = 2.5;
+    cout << "a = " << a << endl; //2.5
+    int b = 2.5;
+    cout << "b = " << b << endl; //2
+    int c = 2 + 3.5;
+    cout << "c = " << c << endl; //5
+    char ch = 97;
+    cout << "ch = " << ch << endl; //a
+    int i = 'a';
+    cout << "i = " << i << endl; //97
+
+    return 0;
+}
