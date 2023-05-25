e9d1b62c6311ac3beff7fcf0d79fc4c7e6d23aab
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-05-25 11:24:07 +0600

    Create XOR.cpp

---
+++ XOR.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 11;
+    int result = a ^ b;
+    cout << result << endl;
+    cout << (4 ^ 4) << endl;
+
+    return 0;
+} 
