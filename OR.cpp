450f867316a5dc9a55709c80de7b7352e8aa51de
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-05-25 11:20:54 +0600

    Create OR.cpp

---
+++ OR.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 3, b = 4;
+    int result = (a | b);
+    cout << result << endl;
+    cout << (0 | 4) << endl;
+
+    return 0;
+}
