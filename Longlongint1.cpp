56694977944fafa0b805441c35470cbdee43fba8
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-05-25 11:34:29 +0600

    Create Longlongint1.cpp

---
+++ Longlongint1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1000000000, b = 1000000000;
+    long long int result = (long long int) a * b;
+    cout << result << '\n';
+    return 0;
+}
