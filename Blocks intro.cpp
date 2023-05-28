d95a3390174e7a7d95dfa1f1c159a28cda373ca8
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-05-28 11:04:05 -0700

    Create Blocks intro.cpp

---
+++ Blocks intro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 5;
+     cout << x << '\n';
+    {
+        int y = 10;
+        cout << y << '\n';
+
+    }
+    
+    return 0;
+}
