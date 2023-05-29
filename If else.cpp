769bf086a602df0f8e334ee8b10bc687e578a9ef
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-05-29 22:50:29 +0600

    Add files via upload

---
+++ If else.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int num;
+   cin >> num;
+   if (num >= 0)
+   {
+       cout << "The number is positive." << '\n';
+   }
+   else
+   {
+       cout << "The number is negative." << '\n';
+   }
+
+    return 0;
+}
+
