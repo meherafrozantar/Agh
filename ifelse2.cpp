deca7144f69cf9649f3bdb03cdf4ee832c6f361d
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-06-01 10:50:16 -0700

    Create ifelse2.cpp

---
+++ ifelse2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 1;
+    if(x >= 0)
+    {
+        cout << "x is at least 0\n";
+    }
+    if(x >= 1)
+    {
+        cout << "x is at least 1\n";
+    }
+    else
+    {
+        cout << "x is less than 0\n";
+    }
+
+    return 0;
+}
