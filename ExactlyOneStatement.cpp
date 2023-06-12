429ad172462d6f8f2ea72b010bb10d1fa16cf0ba
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-06-11 23:29:20 -0700

    Create ExactlyOneStatement.cpp

---
+++ ExactlyOneStatement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 3;
+    if (x ==0)
+        cout << "x is 0\n";
+    else
+        cout << "x is not 0\n";
+
+    return 0;
+}
