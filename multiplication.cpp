628222ba74655c094b3da2948c1749c746411cfa
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-06-15 09:12:02 -0700

    Create multiplication.cpp

---
+++ multiplication.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= 10; i++) {
+        cout << n << " * " << i << " = " << n * i << '\n';
+    }
+    return 0;
+}
