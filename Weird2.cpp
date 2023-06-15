2dca1d5b8fe8c39ea612c1f994640cb9f06b17d2
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-06-15 09:03:58 -0700

    Create Weird2.cpp

---
+++ Weird2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 4;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << ' ';
+    }
+
+    cout << '\n';
+    cout << i << '\n';
+        return 0;
+}
