91ea827039138a017d250096052c62b2a878ff1f
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-06-15 04:47:22 -0700

    Create SumofNumbersFor.cpp

---
+++ SumofNumbersFor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 10, sum = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        sum += i;
+    }
+    cout << sum << '\n';
+
+    return 0;
+}
