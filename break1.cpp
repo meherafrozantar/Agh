62ae067366b53c1db0cb1cbe3da910b61929fb58
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-06-17 10:51:54 -0700

    Create break1.cpp

---
+++ break1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    while (true)
+    {
+        cin >> num;
+        if (num == 7)
+        {
+            break;
+        }
+        cout << num << '\n';
+    }
+   return 0;
+}
