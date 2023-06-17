34455e7623a3143df88d788126dfa41c950f218c
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-06-17 11:12:50 -0700

    Create EvenNumbersForLoop.cpp

---
+++ EvenNumbersForLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << '\n';
+        }
+    }
+
+   return 0;
+}
