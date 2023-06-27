954641ddb04bf000298d1ea704ef53346f251a16
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-06-27 10:26:01 -0700

    Create break2NestedLoop.cpp

---
+++ break2NestedLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int week = 1; week <= 4; week++)
+    {
+        cout << "week = " << week << '\n';
+        for (int day = 1; day <= 7; day++)
+        {
+            if (day == 6) break;
+            cout << " day = "<< day << '\n';
+        }
+    }
+        return 0;
+}
+
