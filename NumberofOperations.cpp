89a20079f32bf7d7037be1f4bf3834b3d98b1834
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-06-28 00:43:34 -0700

    Create NumberofOperations.cpp

---
+++ NumberofOperations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int operation = 0;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 6; j++)
+        {
+            operation ++;
+        }
+    }
+    cout << operation ;
+    return 0;
+}
