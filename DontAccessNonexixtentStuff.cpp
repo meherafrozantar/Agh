f9495c0bd8d2c687ef7ee2625e52c8032cfd0f29
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-07-04 09:09:52 -0700

    Create DontAccessNonexixtentStuff.cpp

---
+++ DontAccessNonexixtentStuff.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age[5] = {1, 2, 3, 4, 5};
+    cout << age[3] << '\n';
+    cout << age[5] << '\n';
+    cout << age[10] << '\n';
+    cout << age[-1] << '\n';
+    return 0;
+}
