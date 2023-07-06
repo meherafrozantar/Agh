57333398684310f9f78468e679cf8994f01ea3ee
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-07-06 09:10:29 -0700

    Create OccurrenceEasy.cpp

---
+++ OccurrenceEasy.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 6;
+    int a[n] = {1, 2, 2, 9, 1, 1};
+    for (int number = 1; number <= 10; number++) {
+        int number_count = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == number) {
+                number_count++;
+            }
+        }
+        cout << number << " occurs "  << number_count << " times\n";
+    }
+    return 0;
+}
