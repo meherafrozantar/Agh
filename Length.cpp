25d77243705afff3562d7f5fe7a12585a3975ae6
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-07-12 06:17:18 -0700

    Create Length.cpp

---
+++ Length.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+    char name[100];
+    cin >> name;
+    int len = strlen(name);
+    cout << len << '\n';
+    return 0;
+}
