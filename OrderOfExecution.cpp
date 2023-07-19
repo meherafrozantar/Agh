cd2a0ffb2cbb4b3f410ad371c69f11ad70e9338c
Meher Afroz Antor <meherafrozantor@gmail.com>
2023-07-19 08:57:05 -0700

    Create OrderOfExecution.cpp

---
+++ OrderOfExecution.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void hey()
+{
+    cout << "Hey man!" << endl;
+}
+int main()
+{
+    cout << "Start\n";
+    hey();
+    cout << "Mid\n";
+    hey();
+    cout << "End\n";
+    return 0;
+}
