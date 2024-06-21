3023e70b3727c474b75cb118a02e07402f6e5d25
Meher Afroz Antar <meherafrozantor@gmail.com>
2024-06-21 17:14:40 +0600

    Create Break.cpp

---
+++ Break.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i <= 10; i++){
+        if (i == 6){
+            break;
+        }
+        cout << i << '\n';
+    }
+    return 0;
+}
