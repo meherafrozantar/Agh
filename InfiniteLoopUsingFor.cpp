9a3803a9fc7e1d68129dbeeffb14d183c7664f50
Meher Afroz Antar <meherafrozantor@gmail.com>
2024-06-21 15:55:07 +0600

    Create InfiniteLoopUsingFor.cpp

---
+++ InfiniteLoopUsingFor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 4;
+    for (i = 1; i >= 0; i++){
+        cout << i << '\n' ;
+    }
+    cout << '\n';
+    cout << i;
+    return 0;
+}
